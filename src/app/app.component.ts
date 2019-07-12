b5541bbc21e5cf2de6dae2ea0864a9aee39219c6
unknown <git@mail.tripapic.com>
2019-07-12 20:48:43 +0700

    Update 12-Jun-19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ToolbarService, DocumentEditor,
+  DocumentEditorContainer, DocumentEditorContainerComponent } from '@syncfusion/ej2-angular-documenteditor';
+import { TitleBar } from './title-bar';
+import { defaultDocument, WEB_API_ACTION } from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ToolbarService]
 })
-export class AppComponent {
-  title = 'ng8-syncfusion';
+export class AppComponent implements OnInit {
+  // title = 'angular-syncfusion';
+  public hostUrl: string;
+  // public serviceLink: string;
+  // tslint:disable-next-line:variable-name
+  @ViewChild('documenteditor_default', {static: true}) documenteditor_default: ElementRef<DocumentEditorContainerComponent>;
+  public container: DocumentEditorContainerComponent;
+
+  // tslint:disable-next-line: variable-name
+  @ViewChild('default_title_bar', {static: true}) default_title_bar: ElementRef<HTMLElement>;
+  titleBar: TitleBar;
+
+  ngOnInit() {
+    // this.serviceLink = 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/import';
+    this.hostUrl = 'https://ej2services.syncfusion.com/production/web-services/';
+    console.log('container', this.container);
+  }
+
+  onCreate(): void {
+    const titleBarElement: HTMLElement = document.getElementById('default_title_bar');
+    this.titleBar = new TitleBar(titleBarElement, this.container.documentEditor, true);
+    this.container.locale = 'en-US';
+    this.container.serviceUrl = this.hostUrl + WEB_API_ACTION;
+    this.container.documentEditor.open(JSON.stringify(defaultDocument));
+    this.container.documentEditor.documentName = 'Getting Started';
+    this.titleBar.updateDocumentTitle();
+    this.container.documentEditor.documentChange = (): void => {
+        this.titleBar.updateDocumentTitle();
+        this.container.documentEditor.focusIn();
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { DropDownButtonModule, SplitButtonModule } from '@syncfusion/ej2-angular-splitbuttons';
+import { DocumentEditorAllModule, DocumentEditorContainerAllModule } from '@syncfusion/ej2-angular-documenteditor';
+import { ToolbarModule, TabModule } from '@syncfusion/ej2-angular-navigations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DialogModule,
+    DropDownButtonModule,
+    SplitButtonModule,
+    DocumentEditorAllModule,
+    DocumentEditorContainerAllModule,
+    ToolbarModule,
+    TabModule
   ],
   providers: [],
   bootstrap: [AppComponent]
